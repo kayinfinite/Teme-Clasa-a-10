7af54a5f96566119e71e1801e9be5fa61afa8352
kayinfinite <123296312+kayinfinite@users.noreply.github.com>
2024-09-28 14:36:50 +0300

    Tema terminata(27.09.24)

--- 27_09_24.cpp
+++ 27_09_24.cpp
@@ -8,11 +8,58 @@ int main() {
     cout << "Cate coloane sa aiba matricea: "; cin >> n ;
     for(i=0; i<m; i++)
         for(j=0; j<n; j++){
-            cout <<"Introduceti numarul natural de pe linia "<< i <<", coloana "<< j << ': ';
+            cout <<"Introduceti numarul natural de pe linia "<< i+1 <<", coloana "<< j+1 << ": ";
             cin >> v[i][j];
         }
 
+    //afisam matricea
+    
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++)
+            cout << v[i][j] << ' ';
+        cout << '\n';
+    }
 
+    //citim ce lini si coloane sa scimbam
 
+    cout <<"Ce care e prima linie pe care o schimi: "; cin >> l1; l1--;
+    cout <<"Ce care e a doua linie pe care o schimi: "; cin >> l2; l2--;
+    cout <<"Ce care e prima coloana pe care o schimi: "; cin >> c1; c1--;
+    cout <<"Ce care e a doua coloana pe care o schimi: "; cin >> c2; c2--;
+    
+    //Schimbam linile l1 cu l2
+    
+    for ( j = 0; j<n; j++){
+       temp = v[l1][j];
+       v[l1][j] = v[l2][j];
+       v[l2][j] = temp; 
+    }
+
+    //afisam matricea
+
+    cout << "Am schimbat linile: \n";
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++)
+            cout << v[i][j] << ' ';
+        cout << '\n';
+    }
+
+    //Schimbam coloanele c1 cu c2
+
+    for ( i = 0; i<m; i++){
+        temp = v[i][c1];
+        v[i][c1] = v[i][c2];
+        v[i][c2] = temp; 
+    }
+
+    //afisam matricea
+
+    cout << "Am schimbat coloanele: \n";
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++)
+            cout << v[i][j] << ' ';
+        cout << '\n';
+    }
+    
     return 0;
 }
